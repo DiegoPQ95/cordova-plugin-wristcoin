010bffe3d80e9f3b1492e3ffdfe4a2787ae163f6
Diego Paguay <gosuetian@hotmail.es>
2025-01-24 15:56:40 +0000

    Correcion en el nombre de la acción

--- www/index.js
+++ www/index.js
@@ -2,9 +2,9 @@ var exec = require('cordova/exec');
 
 var WristCoin = {
     // Función para invocar la funcionalidad nativa de Android/iOS
-    connect: function (successCallback, errorCallback) {
+    initializeDevice: function (successCallback, errorCallback) {
         // Llamada a la función nativa en el dispositivo (a través de exec)
-        exec(successCallback, errorCallback, 'WristCoin', 'connect', []);
+        exec(successCallback, errorCallback, 'WristCoin', 'initializeDevice', []);
     }
     , readWristBand: function (successCallback, errorCallback) {
         exec(successCallback, errorCallback, 'WristCoin', 'readWristBand', []);
